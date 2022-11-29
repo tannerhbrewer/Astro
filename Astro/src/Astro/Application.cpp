ae794588fc33b53b320034f3f429e278e3c85685
Tanner Brewer <tannerhbrewer@gmail.com>
2022-11-29 12:31:27 -0500

    event system

--- Astro/src/Astro/Application.cpp
+++ Astro/src/Astro/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Events/ApplicationEvent.h"
+#include "Log.h"
+
 namespace Astro {
 
 	Application::Application()
@@ -13,6 +16,16 @@ namespace Astro {
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			AS_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			AS_TRACE(e);
+		}
+
 		while (true);
 	}
 
--- Astro/src/Astro/Core.h
+++ Astro/src/Astro/Core.h
@@ -11,3 +11,5 @@
 #else
 	#error Astro only supports Windows!
 #endif
+
+#define BIT(x) (1 << x)

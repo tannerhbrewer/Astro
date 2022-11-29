07bdf4a310b1e7aceab63e17046e5952231b28bb
Tanner Brewer <tannerhbrewer@gmail.com>
2022-11-29 13:45:17 -0500

    window events

--- Astro/src/Astro/Application.cpp
+++ Astro/src/Astro/Application.cpp
@@ -1,20 +1,32 @@
 #include "aspch.h"
 #include "Application.h"
 
-#include "Astro/Events/ApplicationEvent.h"
 #include "Astro/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Astro {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
+
 
 	Application::~Application()
 	{
+
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		AS_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -25,6 +37,12 @@ namespace Astro {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 }

151fc8b25b723354768edb9d2b7f7be1724db510
Tanner Brewer <tannerhbrewer@gmail.com>
2022-12-14 12:31:06 -0500

    material systems

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,8 +1,11 @@
 #include <Astro.h>
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class ExampleLayer : public Astro::Layer
 {
@@ -86,16 +89,16 @@ public:
 			}
 		)";
 
-		m_Shader.reset(new Astro::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(Astro::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueShaderVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
-			out vec3 v_Position;
 
 			uniform mat4 u_ViewProjection;
 			uniform mat4 u_Transform;
+			out vec3 v_Position;
 
 			void main()
 			{
@@ -104,18 +107,20 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
 			in vec3 v_Position;
+			
+			uniform vec3 u_Color;
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}
 		)";
 
-		m_BlueShader.reset(new Astro::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(Astro::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(Astro::Timestep ts) override
@@ -142,13 +147,17 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		std::dynamic_pointer_cast<Astro::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<Astro::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
+
 		for (int y = 0; y < 15; y++)
 		{
 			for (int x = 0; x < 15; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				Astro::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				Astro::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
@@ -159,6 +168,9 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(Astro::Event& event) override
@@ -187,7 +199,7 @@ private:
 	std::shared_ptr<Astro::Shader> m_Shader;
 	std::shared_ptr<Astro::VertexArray> m_VertexArray;
 
-	std::shared_ptr<Astro::Shader> m_BlueShader;
+	std::shared_ptr<Astro::Shader> m_FlatColorShader;
 	std::shared_ptr<Astro::VertexArray> m_SquareVA;
 
 	Astro::OrthographicCamera m_Camera;
@@ -195,6 +207,8 @@ private:
 	float m_CameraSpeed = 2.0f;
 
 	glm::vec3 m_SquarePosition;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 class Sandbox : public Astro::Application

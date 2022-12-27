66b47e7283430c3e18232cc41f6029957e3d5218
tannerhbrewer <tannerhbrewer@gmail.com>
2022-12-27 16:00:03 -0500

    basic profiling

--- Astro/src/Astro/ImGui/ImGuiLayer.h
+++ Astro/src/Astro/ImGui/ImGuiLayer.h
@@ -16,7 +16,6 @@ namespace Astro {
 
 		virtual void OnAttach() override;
 		virtual void OnDetach() override;
-		virtual void OnImGuiRender() override;
 
 		void Begin();
 		void End();
--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -23,6 +23,14 @@ private:
 
 	Astro::Ref<Astro::Texture2D> m_CheckerboardTexture;
 
+	struct ProfileResult
+	{
+		const char* Name;
+		float Time;
+	};
+
+	std::vector<ProfileResult> m_ProfileResults;
+
 	glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
 	glm::vec3 m_SquarePosition = { 0.0f, 0.0f, 0.0f};
 };

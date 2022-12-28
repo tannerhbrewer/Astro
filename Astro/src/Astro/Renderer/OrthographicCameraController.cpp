354063dd664d86a305b28c4faeccdb2e2f6e4d26
tannerhbrewer <tannerhbrewer@gmail.com>
2022-12-28 12:39:15 -0500

    changed camera controller to new keycode enums

--- Astro/src/Astro/Renderer/OrthographicCameraController.cpp
+++ Astro/src/Astro/Renderer/OrthographicCameraController.cpp
@@ -15,23 +15,23 @@ namespace Astro {
 	{
 		AS_PROFILE_FUNCTION();
 
-		if (Input::IsKeyPressed(AS_KEY_A))
+		if (Input::IsKeyPressed(KeyCode::A))
 		{
 			m_CameraPosition.x -= cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 			m_CameraPosition.y -= sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 		}
-		else if (Input::IsKeyPressed(AS_KEY_D))
+		else if (Input::IsKeyPressed(KeyCode::D))
 		{
 			m_CameraPosition.x += cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 			m_CameraPosition.y += sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 		}
 
-		if (Input::IsKeyPressed(AS_KEY_W))
+		if (Input::IsKeyPressed(KeyCode::W))
 		{
 			m_CameraPosition.x += -sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 			m_CameraPosition.y += cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 		}
-		else if (Input::IsKeyPressed(AS_KEY_S))
+		else if (Input::IsKeyPressed(KeyCode::S))
 		{
 			m_CameraPosition.x -= -sin(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
 			m_CameraPosition.y -= cos(glm::radians(m_CameraRotation)) * m_CameraTranslationSpeed * ts;
@@ -39,9 +39,9 @@ namespace Astro {
 
 		if (m_Rotation)
 		{
-			if (Input::IsKeyPressed(AS_KEY_Q))
+			if (Input::IsKeyPressed(KeyCode::Q))
 				m_CameraRotation += m_CameraRotationSpeed * ts;
-			if (Input::IsKeyPressed(AS_KEY_E))
+			if (Input::IsKeyPressed(KeyCode::E))
 				m_CameraRotation -= m_CameraRotationSpeed * ts;
 
 			if (m_CameraRotation > 180.0f)

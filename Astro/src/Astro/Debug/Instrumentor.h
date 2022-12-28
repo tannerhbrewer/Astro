fd761162ca343604ed79e241a2ae83c37fcb9146
tannerhbrewer <tannerhbrewer@gmail.com>
2022-12-28 12:54:28 -0500

    updated instrumentor to deal with multithreading

--- Astro/src/Astro/Debug/Instrumentor.h
+++ Astro/src/Astro/Debug/Instrumentor.h
@@ -1,10 +1,9 @@
 #pragma once
 
-#include <string>
-#include <chrono>
 #include <algorithm>
+#include <chrono>
 #include <fstream>
-
+#include <string>
 #include <thread>
 
 namespace Astro {
@@ -12,7 +11,7 @@ namespace Astro {
 	{
 		std::string Name;
 		long long Start, End;
-		uint32_t ThreadID;
+		std::thread::id ThreadID;
 	};
 
 	struct InstrumentationSession
@@ -23,55 +22,76 @@ namespace Astro {
 	class Instrumentor
 	{
 	private:
+		std::mutex m_Mutex;
 		InstrumentationSession* m_CurrentSession;
 		std::ofstream m_OutputStream;
-		int m_ProfileCount;
 	public:
 		Instrumentor()
-			: m_CurrentSession(nullptr), m_ProfileCount(0)
+			: m_CurrentSession(nullptr)
 		{
 		}
 
 		void BeginSession(const std::string& name, const std::string& filepath = "results.json")
 		{
+			std::lock_guard lock(m_Mutex);
+			if (m_CurrentSession) {
+				if (Log::GetCoreLogger()) {
+					AS_CORE_ERROR("Instrumentor::BeginSession('{0}') when session '{1}' already open.", name, m_CurrentSession->Name);
+				}
+				InternalEndSession();
+			}
 			m_OutputStream.open(filepath);
-			WriteHeader();
-			m_CurrentSession = new InstrumentationSession{ name };
+			if (m_OutputStream.is_open()) {
+				m_CurrentSession = new InstrumentationSession({ name });
+				WriteHeader();
+			}
+			else {
+				if (Log::GetCoreLogger()) {
+					AS_CORE_ERROR("Instrumentor could not open results file '{0}'.", filepath);
+				}
+			}
 		}
 
 		void EndSession()
 		{
-			WriteFooter();
-			m_OutputStream.close();
-			delete m_CurrentSession;
-			m_CurrentSession = nullptr;
-			m_ProfileCount = 0;
+			std::lock_guard lock(m_Mutex);
+			InternalEndSession();
 		}
 
 		void WriteProfile(const ProfileResult& result)
 		{
-			if (m_ProfileCount++ > 0)
-				m_OutputStream << ",";
+			std::stringstream json;
 
 			std::string name = result.Name;
 			std::replace(name.begin(), name.end(), '"', '\'');
 
-			m_OutputStream << "{";
-			m_OutputStream << "\"cat\":\"function\",";
-			m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
-			m_OutputStream << "\"name\":\"" << name << "\",";
-			m_OutputStream << "\"ph\":\"X\",";
-			m_OutputStream << "\"pid\":0,";
-			m_OutputStream << "\"tid\":" << result.ThreadID << ",";
-			m_OutputStream << "\"ts\":" << result.Start;
-			m_OutputStream << "}";
+			json << ",{";
+			json << "\"cat\":\"function\",";
+			json << "\"dur\":" << (result.End - result.Start) << ',';
+			json << "\"name\":\"" << name << "\",";
+			json << "\"ph\":\"X\",";
+			json << "\"pid\":0,";
+			json << "\"tid\":" << result.ThreadID << ",";
+			json << "\"ts\":" << result.Start;
+			json << "}";
 
-			m_OutputStream.flush();
+			std::lock_guard lock(m_Mutex);
+			if (m_CurrentSession) {
+				m_OutputStream << json.str();
+				m_OutputStream.flush();
+			}
 		}
+
+		static Instrumentor& Get() {
+			static Instrumentor instance;
+			return instance;
+		}
+
+	private:
 
 		void WriteHeader()
 		{
-			m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
+			m_OutputStream << "{\"otherData\": {},\"traceEvents\":[{}";
 			m_OutputStream.flush();
 		}
 
@@ -81,11 +101,15 @@ namespace Astro {
 			m_OutputStream.flush();
 		}
 
-		static Instrumentor& Get()
-		{
-			static Instrumentor instance;
-			return instance;
+		void InternalEndSession() {
+			if (m_CurrentSession) {
+				WriteFooter();
+				m_OutputStream.close();
+				delete m_CurrentSession;
+				m_CurrentSession = nullptr;
+			}
 		}
+
 	};
 
 	class InstrumentationTimer
@@ -110,8 +134,7 @@ namespace Astro {
 			long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
 			long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
 
-			uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
-			Instrumentor::Get().WriteProfile({ m_Name, start, end, threadID });
+			Instrumentor::Get().WriteProfile({ m_Name, start, end, std::this_thread::get_id() });
 
 			m_Stopped = true;
 		}
@@ -124,13 +147,31 @@ namespace Astro {
 
 #define AS_PROFILE 1
 #if AS_PROFILE
-#define AS_PROFILE_BEGIN_SESSION(name, filepath) ::Astro::Instrumentor::Get().BeginSession(name, filepath)
-#define AS_PROFILE_END_SESSION() ::Astro::Instrumentor::Get().EndSession()
-#define AS_PROFILE_SCOPE(name) ::Astro::InstrumentationTimer timer##__LINE__(name);
-#define AS_PROFILE_FUNCTION() AS_PROFILE_SCOPE(__FUNCSIG__)
+	#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+		#define AS_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__DMC__) && (__DMC__ >= 0x810)
+		#define AS_FUNC_SIG __PRETTY_FUNCTION__
+	#elif defined(__FUNCSIG__)
+		#define AS_FUNC_SIG __FUNCSIG__
+	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+		#define AS_FUNC_SIG __FUNCTION__
+	#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+		#define AS_FUNC_SIG __FUNC__
+	#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+		#define AS_FUNC_SIG __func__
+	#elif defined(__cplusplus) && (__cplusplus >= 201103)
+		#define AS_FUNC_SIG __func__
+	#else
+		#define AS_FUNC_SIG "AS_FUNC_SIG unknown!"
+	#endif
+
+	#define AS_PROFILE_BEGIN_SESSION(name, filepath) ::Astro::Instrumentor::Get().BeginSession(name, filepath)
+	#define AS_PROFILE_END_SESSION() ::Astro::Instrumentor::Get().EndSession()
+	#define AS_PROFILE_SCOPE(name) ::Astro::InstrumentationTimer timer##__LINE__(name);
+	#define AS_PROFILE_FUNCTION() AS_PROFILE_SCOPE(AS_FUNC_SIG)
 #else
-#define AS_PROFILE_BEGIN_SESSION(name, filepath)
-#define AS_PROFILE_END_SESSION()
-#define AS_PROFILE_SCOPE(name)
-#define AS_PROFILE_FUNCTION()
+	#define AS_PROFILE_BEGIN_SESSION(name, filepath)
+	#define AS_PROFILE_END_SESSION()
+	#define AS_PROFILE_SCOPE(name)
+	#define AS_PROFILE_FUNCTION()
 #endif
